6b425c49054b564ab5be6ffbf0a91143665f4d2b
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-08-29 09:37:06 +0530

    Java Kata

---
+++ Java/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/DiagnosticData.java
@@ -0,0 +1,72 @@
+package tddmicroexercises.telemetrysystem;
+
+import java.util.Random;
+
+public class DiagnosticData implements IDiagnostic{
+
+
+    private String diagnosticMessageResult = "";
+
+    private final Random connectionEventsSimulator = new Random(42);
+
+    public void send(String message) {
+        if (message == null || "".equals(message))
+        {
+            throw new IllegalArgumentException();
+        }
+
+        if (message.equals(DIAGNOSTIC_MESSAGE))
+        {
+            // simulate a status report
+            setDiagnosticMessageResult(
+                    "LAST TX rate................ 100 MBPS\r\n"
+                            + "HIGHEST TX rate............. 100 MBPS\r\n"
+                            + "LAST RX rate................ 100 MBPS\r\n"
+                            + "HIGHEST RX rate............. 100 MBPS\r\n"
+                            + "BIT RATE.................... 100000000\r\n"
+                            + "WORD LEN.................... 16\r\n"
+                            + "WORD/FRAME.................. 511\r\n"
+                            + "BITS/FRAME.................. 8192\r\n"
+                            + "MODULATION TYPE............. PCM/FM\r\n"
+                            + "TX Digital Los.............. 0.75\r\n"
+                            + "RX Digital Los.............. 0.10\r\n"
+                            + "BEP Test.................... -5\r\n"
+                            + "Local Rtrn Count............ 00\r\n"
+                            + "Remote Rtrn Count........... 00");
+
+            return;
+        }
+    }
+
+    public String receive()
+    {
+        String message;
+
+        if (getDiagnosticMessageResult() == null || "".equals(getDiagnosticMessageResult()))
+        {
+            // simulate a received message (just for illustration - not needed for this exercise)
+            message = "";
+            int messageLength = connectionEventsSimulator.nextInt(50) + 60;
+            for(int i = messageLength; i >=0; --i)
+            {
+                message += (char)connectionEventsSimulator.nextInt(40) + 86;
+            }
+
+        }
+        else
+        {
+            message = getDiagnosticMessageResult();
+            setDiagnosticMessageResult("");
+        }
+
+        return message;
+    }
+
+    public String getDiagnosticMessageResult() {
+        return diagnosticMessageResult;
+    }
+
+    public void setDiagnosticMessageResult(String diagnosticMessageResult) {
+        this.diagnosticMessageResult = diagnosticMessageResult;
+    }
+}
