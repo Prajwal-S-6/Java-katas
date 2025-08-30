7b0c62ade8f8d6e6875118ca9b8d0d7075f8dbad
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-08-30 14:59:07 +0530

    Java Kata

---
+++ Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Sensor.java
@@ -0,0 +1,28 @@
+package tddmicroexercises.tirepressuremonitoringsystem;
+
+import java.util.Random;
+
+// The reading of the pressure value from the sensor is simulated in this implementation.
+// Because the focus of the exercise is on the other class.
+
+public class Sensor implements ISensor<Double> {
+    public static final double OFFSET = 16;
+
+    public double popNextPressurePsiValue() {
+        double pressureTelemetryValue;
+        pressureTelemetryValue = samplePressure( );
+
+        return OFFSET + pressureTelemetryValue;
+    }
+
+    private static double samplePressure() {
+        // placeholder implementation that simulate a real sensor in a real tire
+        Random basicRandomNumbersGenerator = new Random();
+        return 6 * basicRandomNumbersGenerator.nextDouble() * basicRandomNumbersGenerator.nextDouble();
+    }
+
+    @Override
+    public Double getSensorValue() {
+        return popNextPressurePsiValue();
+    }
+}
