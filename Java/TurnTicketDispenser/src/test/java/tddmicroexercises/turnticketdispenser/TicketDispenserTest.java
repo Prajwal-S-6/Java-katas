80f1f3ffcde281ff3b8d670c475d61d16116ab26
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-02 09:33:49 +0530

    Java Kata

---
+++ Java/TurnTicketDispenser/src/test/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -0,0 +1,38 @@
+package tddmicroexercises.turnticketdispenser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class TicketDispenserTest {
+
+
+    private final MockTurnNumberSequence mockTurnNumber = new MockTurnNumberSequence();
+    private final TicketDispenser ticketDispenser = new TicketDispenser(mockTurnNumber);
+
+
+    @Test
+    void foo() {
+        TurnTicket ticket = ticketDispenser.getTurnTicket();
+        assertEquals(0, ticket.getTurnNumber());
+    }
+
+
+
+
+}
+
+class MockTurnNumberSequence implements ITurnNumberGenerator<Integer>{
+
+    private int turnNumber = 0;
+    @Override
+    public Integer getNextTurnNumber() {
+        return turnNumber++;
+    }
+}
