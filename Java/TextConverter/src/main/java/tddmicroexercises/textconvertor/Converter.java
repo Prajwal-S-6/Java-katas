8f174a6a3ccdd7a4ab1304e91a29a4f577f13dc1
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-06 18:28:27 +0530

    Java Kata

--- Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/Converter.java
+++
@@ -1,9 +0,0 @@
-package tddmicroexercises.textconvertor;
-
-import java.io.*;
-
-public interface Converter {
-
-    Reader getReader() throws FileNotFoundException;
-    String convert(int page) throws IOException;
-}
