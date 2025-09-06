1daf7981ba7714c662447d90efcae4cd5bbdaeb4
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-06 17:36:57 +0530

    Java Kata

--- Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlPagesConverterTest.java
+++ Java/TextConverter/src/test/java/tddmicroexercises/textconvertor/HtmlPagesConverterTest.java
@@ -3,6 +3,7 @@ package tddmicroexercises.textconvertor;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -12,25 +13,30 @@ import java.io.IOException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.util.StringUtils;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class HtmlPagesConverterTest {
 
     HtmlTextConverter mockHtmlConverter;
+    HtmlPagesConverter pagesConverter;
 
     @BeforeEach
     void setUp() {
         mockHtmlConverter = mock(HtmlTextConverter.class);
+        pagesConverter = new HtmlPagesConverter("foo", mockHtmlConverter);
     }
 
     @Test
-    public void foo() throws IOException {
-        HtmlPagesConverter pagesConverter = new HtmlPagesConverter("foo", mockHtmlConverter);
+    public void foo() {
         assertEquals("foo", pagesConverter.getFilename());
     }
 
     @Test
-    public
+    public void shouldReturnEmptyStringWhenPageisEmpty() throws IOException {
+        when(mockHtmlConverter.convert(0)).thenReturn("");
+
+    }
 
 }
