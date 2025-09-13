44b5202d19030909abbf12d358693f038c08c70f
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-13 17:24:10 +0530

    R removed type parameter from parrot class

--- Java/ParentRefactoringKata/src/main/java/com/java/katas/EuropeanParrot.java
+++ Java/ParentRefactoringKata/src/main/java/com/java/katas/EuropeanParrot.java
@@ -3,7 +3,7 @@ package com.java.katas;
 public class EuropeanParrot extends Parrot {
 
     public EuropeanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(ParrotTypeEnum.EUROPEAN, numberOfCoconuts, voltage, isNailed);
+        super(numberOfCoconuts, voltage, isNailed);
     }
 
     @Override
--- Java/ParentRefactoringKata/src/main/java/com/java/katas/NorwegianBlueParrot.java
+++ Java/ParentRefactoringKata/src/main/java/com/java/katas/NorwegianBlueParrot.java
@@ -3,7 +3,7 @@ package com.java.katas;
 public class NorwegianBlueParrot extends Parrot {
 
     public NorwegianBlueParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(ParrotTypeEnum.NORWEGIAN_BLUE, numberOfCoconuts, voltage, isNailed);
+        super(numberOfCoconuts, voltage, isNailed);
     }
 
     @Override
