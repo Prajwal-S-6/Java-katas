3ed1b4b16a0823b4753bfe00fef93ba437146351
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-13 17:34:35 +0530

    r moved createParrot factory method to enum class

--- Java/ParentRefactoringKata/src/main/java/com/java/katas/Parrot.java
+++ Java/ParentRefactoringKata/src/main/java/com/java/katas/Parrot.java
@@ -5,17 +5,6 @@ public abstract class Parrot {
     public Parrot() {
     }
 
-    public static Parrot createParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
-        Parrot parrot;
-        switch (type) {
-            case EUROPEAN -> parrot = new EuropeanParrot();
-            case AFRICAN -> parrot = new AfricanParrot(numberOfCoconuts);
-            case NORWEGIAN_BLUE -> parrot = new NorwegianBlueParrot(voltage, isNailed);
-            default -> throw new IllegalArgumentException("Invalid Parrot type");
-        };
-        return parrot;
-    }
-
     public abstract double getSpeed();
 
     protected double getBaseSpeed(double voltage) {
--- Java/ParentRefactoringKata/src/main/java/com/java/katas/ParrotTypeEnum.java
+++ Java/ParentRefactoringKata/src/main/java/com/java/katas/ParrotTypeEnum.java
@@ -4,5 +4,16 @@ public enum ParrotTypeEnum {
 
     EUROPEAN,
     AFRICAN,
-    NORWEGIAN_BLUE
+    NORWEGIAN_BLUE;
+
+    public static Parrot createParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        Parrot parrot;
+        switch (type) {
+            case EUROPEAN -> parrot = new EuropeanParrot();
+            case AFRICAN -> parrot = new AfricanParrot(numberOfCoconuts);
+            case NORWEGIAN_BLUE -> parrot = new NorwegianBlueParrot(voltage, isNailed);
+            default -> throw new IllegalArgumentException("Invalid Parrot type");
+        };
+        return parrot;
+    }
 }
