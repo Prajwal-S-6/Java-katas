74cbe8663f1fa3433e5fa6781aad3b5de08444d2
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-14 09:48:36 +0530

    r renamed reduce quality method

--- Java/GlidedRose/src/main/java/com/java/katas/GildedRose.java
+++ Java/GlidedRose/src/main/java/com/java/katas/GildedRose.java
@@ -9,7 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            reduceQualityByOneForNonAgedBrieBackstageSulfurusNotLessThanZero(i);
+            reduceQualityByTwoIfNotAgedBrieBackstageSulfurusAndLessThanZero(i);
             increaseQualityNotMoreThan50ForAgedBrieOrBackstage(i);
             increaseQualityForBackstageItemBasedOnSellIn(i);
             reduceSellInForNonSulfurusItem(i);
@@ -45,7 +45,7 @@ class GildedRose {
         }
     }
 
-    private void reduceQualityByOneForNonAgedBrieBackstageSulfurusNotLessThanZero(int i) {
+    private void reduceQualityByTwoIfNotAgedBrieBackstageSulfurusAndLessThanZero(int i) {
         if (items[i].quality > 0 && !isSulfurus(i) && !isAgedBrie(i) && !isBackstage(i)) {
             items[i].quality = items[i].quality - 1;
         }
@@ -66,7 +66,7 @@ class GildedRose {
             increaseQualityNotMoreThan50ForAgedBrieOrBackstage(i);
             reduceQualityToZeroForBackStageItem(i);
         }
-        reduceQualityByOneForNonAgedBrieBackstageSulfurusNotLessThanZero(i);
+        reduceQualityByTwoIfNotAgedBrieBackstageSulfurusAndLessThanZero(i);
     }
 
     private void reduceQualityToZeroForBackStageItem(int i) {
