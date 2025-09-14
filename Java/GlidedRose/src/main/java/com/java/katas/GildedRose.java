8e8f69af6dbc210696ef7b23bbe7b889ffc3b67c
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-09-14 08:38:01 +0530

    r extracted method to reduce quality

--- Java/GlidedRose/src/main/java/com/java/katas/GildedRose.java
+++ Java/GlidedRose/src/main/java/com/java/katas/GildedRose.java
@@ -12,9 +12,7 @@ class GildedRose {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+                    reduceQualityForNonSulfurusItem(i);
                 }
             } else {
                 if (items[i].quality < 50) {
@@ -42,9 +40,7 @@ class GildedRose {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+                            reduceQualityForNonSulfurusItem(i);
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
@@ -59,6 +55,12 @@ class GildedRose {
                     items[i].quality = items[i].quality - 1;
                 }
             }
+        }
+    }
+
+    private void reduceQualityForNonSulfurusItem(int i) {
+        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            items[i].quality = items[i].quality - 1;
         }
     }
 
